eb2fe709ee5790d39473ba4aa852c387773641af
roystanalva <roystanalva@gmail.com>
2019-06-07 08:36:47 +0530

    first commit

---
+++ Test/Samp1.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium
+browser='chrome'
+if(browser=='chrome'):
+    driver =  webdriver.Chrome(executable_path='C:/Users/Roy/Downloads/Driver/chromedriver.exe')
+elif(browser=='firefox'):
+    driver = webdriver.Firefox(executable_path='C:/Users/Roy/Downloads/Driver/geckodriver.exe')
+else:
+    print("No browser path found")
+driver.get('http://www.facebook.com')
+driver.implicitly_wait(30)
+driver.find_element_by_name("firstname").send_keys("Harish")
+driver.find_element_by_name("lastname").send_keys("Qspider")
+driver.find_element_by_xpath("//input[contains(@")
