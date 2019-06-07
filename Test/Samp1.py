4a33629b2721c69d59b301456788f23aaf98ca62
roystanalva <roystanalva@gmail.com>
2019-06-07 10:53:22 +0530

    3rd Push

--- Test/Samp1.py
+++ Test/Samp1.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium
 browser='chrome'
 if(browser=='chrome'):
     driver =  webdriver.Chrome(executable_path='C:/Users/Roy/Downloads/Driver/chromedriver.exe')
@@ -9,6 +8,19 @@ else:
     print("No browser path found")
 driver.get('http://www.facebook.com')
 driver.implicitly_wait(30)
-driver.find_element_by_name("firstname").send_keys("Harish")
-driver.find_element_by_name("lastname").send_keys("Qspider")
+driver.find_element_by_name("firstname").send_keys("Test Name")
+driver.find_element_by_name("lastname").send_keys("Test Surname")
 #driver.find_element_by_xpath("//input[contains(@//*[@id="u_0_q"]")
+driver.find_element_by_xpath("//*[@id='u_0_q']").send_keys("9876543210")
+driver.find_element_by_xpath("//*[@id='u_0_x']").send_keys("Password@123")
+driver.find_element_by_name("birthday_day").send_keys("24")
+driver.find_element_by_id("month").send_keys("Mar")
+driver.find_element_by_name("birthday_year").send_keys("1992")
+driver.find_element_by_xpath("//input[@value='2']").click()
+driver.find_element_by_name("websubmit").click()
+
+
+
+
+
+
--- Test/jqueryui.py
+++ Test/jqueryui.py
@@ -7,18 +7,19 @@ elif(browser=='firefox'):
 else:
     print("No browser path found")
 
-driver.get("http://jqueryui.com/datepicker/")
+driver.get("https://jqueryui.com/datepicker/#dropdown-month-year")
 driver.implicitly_wait(30)
 #driver.find_element_by_xpath("//*[@id="datepicker"]"]
 framevar= driver.find_element_by_xpath("//iframe'[@class='demo-frame']")
 driver.switch_to_frame(framevar)
 driver.find_element_by_xpath("//input[@class='hasDatepicker']").click()
 month=driver.find_element_by_class_name("ui-datepicker-month")
-s1= Select(month)
+s1=Select(month)
 s1.select_by_visible_text("Mar")
 year=driver.find_element_by_class_name("ui-datepicker-year")
 s2=Select(year)
 s1.select_by_value("2010")
+driver.
 
 
 
